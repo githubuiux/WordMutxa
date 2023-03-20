7d55e941331aa9f7c4aee4eab0b91b132a0b5731
githubuiux <uiuxista@gmail.com>
2023-03-20 10:33:12 +0300

    Initial Commit

---
+++ WordMutxa/WordMutxaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordMutxaApp.swift
+//  WordMutxa
+//
+//  Created by Levent on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordMutxaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
